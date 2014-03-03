4240b11e669d59012e179e8d31c797636731f80b
Damian Cardozo <damian.cardozo@neonroots.com>
2014-03-03 15:39:50 -0200

    creat project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Landingsize::Application.config.secret_key_base = 'a6bc9124fe495501c5f8d9556aed90389f1bc9421af684f4c0ee6ed5833cc9d131b041c713237adb4d6a4855735f99c93175982f99aaa90a686148a01d7cd730'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Landingsize::Application.config.session_store :cookie_store, key: '_landingsize_session'
